794f59e3198af2893099ceeacb0d413d74148ab8
Jesus Pimentel <jesus.pimentel@utp.ac.pa>
2026-05-24 02:48:52 +0000

    Arreglando app.js

--- app.js
+++ app.js
@@ -20,9 +20,7 @@ const { attachLocals } = require('./middleware/authMiddleware');
 const app  = express();
 const port = process.env.PORT || 3000;
 
-app.use('/customer', customerRoutes);
-app.use('/store-admin', storeAdminRoutes);
-app.use('/user', userAuthRoutes);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('layout', 'layout');        // usa views/layout.ejs como plantilla base
@@ -58,10 +56,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/',         productRoutes);
-app.use('/cart',     cartRoutes);
-app.use('/checkout', checkoutRoutes);
-app.use('/store',    storeAuthRoutes);
+app.use('/',           productRoutes);
+app.use('/cart',       cartRoutes);
+app.use('/checkout',   checkoutRoutes);
+app.use('/store',      storeAuthRoutes);
+app.use('/customer',   customerRoutes);
+app.use('/store-admin', storeAdminRoutes);
+app.use('/user',       userAuthRoutes);
 
 app.use((req, res) => {
   res.status(404).render('404', { title: 'Pagina no encontrada' });
